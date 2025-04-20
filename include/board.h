99fc82381f409c82c5e8b4ff28ac5e4e0a2cddf1
davidspiv <daspivack@gmail.com>
2025-04-19 23:45:40 -0700

    centered

--- include/board.h
+++ include/board.h
@@ -12,7 +12,7 @@ struct Board {
   const float tile_size;
 
   Board(unsigned w_width, unsigned w_height, float tile_size)
-      : rows(32), cols(32), tile_size(tile_size) {}
+      : rows(64), cols(64), tile_size(tile_size) {}
 };
 
 struct Tile {
@@ -22,9 +22,9 @@ struct Tile {
   Tile_Role role;
   sf::ConvexShape shape;
 
-  Tile(const sf::Vector2f origin, const float size, Tile_Role role)
+  Tile(const sf::Vector2f origin, const Board& board, Tile_Role role)
       : origin(origin),
-        size(size),
+        size(board.tile_size),
         center(origin.x + size / 2.f, origin.y + size / 2.f),
         role(role),
         shape(sf::ConvexShape(4.f)) {
@@ -37,7 +37,13 @@ struct Tile {
     sf::Vector2f isometric_origin{origin.x - origin.y,
                                   (origin.x + origin.y) / 2.f};
 
-    shape.setPosition(isometric_origin - sf::Vector2f(0.f, (size / 2.f)));
+    const float isometric_board_half_height =
+        (board.cols + board.rows - 1) * (size / 8.f);
+
+    // ~1.7 works, idk why! Golden ratio? Screen aspect ratio?
+    const float y_offset = isometric_board_half_height * 1.7;
+
+    shape.setPosition(isometric_origin - sf::Vector2f(0.f, y_offset));
     shape.setFillColor(sf::Color(0, 0, 0));
     shape.setOutlineThickness(2.f);
     shape.setOutlineColor(sf::Color(250, 150, 100));
@@ -55,7 +61,7 @@ std::vector<Tile> get_tiles(const Board& board, const float screen_width,
     for (size_t col = 0; col < board.cols; ++col) {
       float x = col * board.tile_size + x_offset;
       float y = row * board.tile_size;
-      Tile tile(sf::Vector2f(x, y), board.tile_size, EMPTY);
+      Tile tile(sf::Vector2f(x, y), board, EMPTY);
       tiles.emplace_back(tile);
     }
   }
