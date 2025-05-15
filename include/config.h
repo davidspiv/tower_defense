964d2686029056ca83d811e819feebe9cd9a3f3e
davidspiv <daspivack@gmail.com>
2025-05-14 17:51:11 -0700

    updated main

--- include/config.h
+++ include/config.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 constexpr int SCREEN_WIDTH = 1920;
 constexpr int SCREEN_HEIGHT = 1080;
+sf::Vector2i const SCREEN_DIM(SCREEN_WIDTH, SCREEN_HEIGHT); // px
+sf::Vector2u const GRID_DIM(BOARD_ROWS, BOARD_COLS);        // cells
+constexpr int TARGET_FPS = 60;
+
 constexpr unsigned BOARD_ROWS = 5;
 constexpr unsigned BOARD_COLS = 5;
 constexpr unsigned TILE_SIZE_PX = 100;
-constexpr int TARGET_FPS = 60;
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,10 @@
-#include "../include/Board.h"
 #include "../include/Engine.h"
-#include "../include/Tower.h"
 #include "../include/config.h"
-#include "../include/util.h"
 
 #include <SFML/Graphics.hpp>
-#include <chrono>
-#include <thread> // For std::this_thread::sleep_for
-#include <vector>
 
-
-int main() {
-  const sf::Vector2i screen_dim(SCREEN_WIDTH, SCREEN_HEIGHT); // px
-  const sf::Vector2u grid_dim(BOARD_ROWS, BOARD_COLS);        // cells
-
-  Engine game(screen_dim, grid_dim, TILE_SIZE_PX);
-
-  game.run(TARGET_FPS);
+int main()
+{
+    Engine game(SCREEN_DIM, GRID_DIM, TILE_SIZE_PX);
+    game.run(TARGET_FPS);
 }
